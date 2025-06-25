df9ed8ba0c18f8169d9088ac0871d69d5d3b7837
pc <pc@DESKTOP-OUU9EGI>
2025-06-25 15:34:36 +0300

    Add project files.

---
+++ marialesson two/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace marialesson_two
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String stdname="maria ahmed hirsi";
+            String age = "ten years old";
+           
+            String m_Name, dob, pob, email, sex, status, faculty;
+            int phone;
+            Console.WriteLine("magaca ardaygu waa"+ stdname);
+
+            Console.WriteLine("dada ardaygu waa" + age);
+            Console.WriteLine("magaca ardaygu waa" + stdname);
+        }
+    }
+}
+
